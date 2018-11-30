dc3b9e196cf8a051fe2cb96dcd9aa2768e0c3e9f
chris <christopher.t.bate1@gmail.com>
2018-11-30 13:58:53 -0700

    Cleaned up unpacker

--- unpacker.cpp
+++ unpacker.cpp
@@ -2,25 +2,26 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "unpacker.hpp"
 
 using namespace std;
 
 int main()
 {
-    signed char map[4] = {1, 3, -3, -1};
+    // Default mapping.
+    std::vector<int> map = {1, 3, -3, -1};
     bool complex = true;
     unsigned int channels = 2;
     unsigned int bits = 2;
     string file = "";
+
     vector<string> channelFilenames;
-    vector<ofstream *> outFiles;
-    vector<vector<signed char> *> outputBuffers;
-    //const unsigned char bitmask = 0x03;
+    vector<ofstream> outFiles;
+    vector<vector<int8_t>> outputBuffers;
     const unsigned int bufferSize = 1024000;
 
     try
     {
-
         // Process the settings file.
         std::ifstream infile;
         infile.open("settings.txt", ifstream::in);
@@ -48,21 +49,13 @@ int main()
             if (key == "file")
             {
                 infile >> file;
-                cout<<"Input: "<<file<<std::endl;
+                cout << "Input: " << file << std::endl;
             }
             if (key == "out")
             {
                 infile >> value;
                 channelFilenames.push_back(value);
-                std::cout << "Added "<<value<<" for output "<< std::endl;
-                ofstream *infile1 = new ofstream;
-                infile1->open(value, ofstream::out | ofstream::binary);
-                if(!infile1->is_open()){
-                    throw std::runtime_error("Could not open output file.");
-                }
-                outFiles.push_back(infile1); // Should be copied in
-                vector<signed char> *outBuffer = new vector<signed char>;
-                outputBuffers.push_back(outBuffer);
+                std::cout << "Added " << value << " for output " << std::endl;               
             }
         }
         infile.close();
@@ -71,74 +64,81 @@ int main()
         std::cout << "Channels " << channels << endl
                   << "Bits " << bits << endl
                   << "Complex " << complex << endl;
-        std::cout<<"Map:"<<endl;
-        for(auto i = 0; i < 4; i++){
-            std::cout<<i<<": "<<(signed)map[i]<<std::endl;
+        std::cout << "Map:" << endl;        
+        for (auto i = 0; i < 4; i++)
+        {
+            std::cout << i << ": " << (signed)map[i] << std::endl;
         }
 
+        //   Create unpacker.
+        Unpacker unpacker( map, channels, bits ,true);
+        
+        // Verify number of output files is correct.
         if (channelFilenames.size() != channels)
         {
-            throw runtime_error("Must specify the same number of channels as output filenames");
+            throw std::runtime_error("Must specify the same number of channels as output filenames");
         }
 
+        // Open output files and buffers.
+        outFiles.resize(channels);
+        outputBuffers.resize(channels);
+        unsigned int outIdx = 0;
+        for(auto iter = channelFilenames.begin(); iter!=channelFilenames.end();iter++)
+        {
+            outFiles[outIdx].open( (*iter).c_str(), std::ofstream::binary | std::ofstream::out);
+            if(!outFiles[outIdx].is_open()){
+                throw std::runtime_error("Could not open output file.");
+            }
+            outputBuffers[outIdx].resize(unpacker.GetOutputBufferSize( bufferSize ));
+            outIdx++;
+        }        
+
+        // Open the input file.
         infile.open(file, ifstream::in | ifstream::binary);
-        unsigned char buffer[bufferSize];
+        if(!infile.is_open()){
+            throw std::runtime_error("Error opening file. Does not exist or cannot be opened.");
+        }
 
-        unsigned int channelIdx = 0;
-        unsigned int componentIdx = 0;
+        // Create read buffer and unpacker
+        std::vector<uint8_t> readBuffer;        
+        readBuffer.resize(bufferSize);     
 
-        while (!infile.eof())
+        
+        // Perform unpacking
+        unsigned int total = 0;
+        unsigned int lastPrint = 0;                
+        infile.seekg(0,std::ios_base::end);
+        unsigned int fileSize = infile.tellg();
+        infile.seekg(0,std::ios_base::beg);
+        while (!infile.eof())   
         {
             // read in the bytes;
-            infile.read((char *)buffer, bufferSize);
-            auto len = infile.gcount();
+            infile.read((char *)&readBuffer[0], bufferSize);            
 
-            // For each bytes.
-            for (auto i = 0; i < len; i++)
-            {
-                unsigned char byte = buffer[i];
-                for (unsigned int j = 0; j < 8 / bits; j++)
-                {
-                    unsigned char point = (byte >> (bits * j)) & 0x3;
-                    signed char samplePoint = map[point];
-                    outputBuffers[channelIdx]->push_back(samplePoint);
-                    if (complex)
-                    {
-                        componentIdx = (componentIdx + 1) % 2;
-                        if(componentIdx == 0)
-                        {
-                            channelIdx = (channelIdx+1)%channels;
-                        }
-                    }
-                }
-            }
+            // Unpack.
+            unpacker.Unpack(readBuffer, outputBuffers);
+            total += infile.gcount();            
 
             // Write the data to files.
             auto index = 0;
             for (auto it = outFiles.begin(); it != outFiles.end(); it++)
             {
-                (*it)->write((char*)outputBuffers[index]->data(), outputBuffers[index]->size());
-
-                outputBuffers[index]->clear();
+                (*it).write((char *)outputBuffers[index].data(), outputBuffers[index].size());                
                 index++;
-            }
+            }                        
+            //Percentage
+            if(total-lastPrint > 1000000000)
+            {
+                std::cout<< total<<" bytes processed, "<< (double)total/(double)fileSize <<" percent complete."<<std::endl;
+                lastPrint = total;
+            }            
         }
 
+        // Cleanup
         infile.close();
-
-        while (outFiles.size() > 0)
+        for(auto iter = outFiles.begin(); iter != outFiles.end(); iter++)
         {
-            ofstream *item = outFiles.back();
-            outFiles.pop_back();
-            item->close();
-            delete item;
-        }
-
-        while (outputBuffers.size() > 0)
-        {
-            vector<signed char> *item = outputBuffers.back();
-            outputBuffers.pop_back();
-            delete item;
+            (*iter).close();
         }
     }
     catch (exception &e)
---
+++ unpacker.hpp
@@ -0,0 +1,103 @@
+#ifndef UNPACKER_H
+#define UNPACKER_H
+
+#include <vector>
+#include <cstdint>
+#include <exception>
+
+/**
+ * Unpacker class
+ */
+class Unpacker
+{
+  public:
+    /**
+     * Init with parameters
+     */
+    Unpacker(std::vector<int> &map, unsigned int numChannels, unsigned int numBits,bool complex) : m_map(map), m_numChannels(numChannels), m_numBits(numBits),m_isComplex(complex)
+    {        
+        ResetIdicies();
+    }
+
+    /**
+     * Defaults
+     */
+    Unpacker()
+    {
+        m_isComplex = true;
+        m_map = {1, 3, -3, -1};
+        m_numChannels = 2;
+        m_numBits = 2;
+        ResetIdicies();
+    }
+
+    void ResetIdicies()
+    {
+        m_componentIdx = m_channelIdx = 0;
+    }
+
+    double GetChanSamplesPerByte()
+    {
+        return ((double)(8 / m_numBits) / (double)(m_isComplex ? 2 : 1) / (double)m_numChannels) ;
+    }
+
+    unsigned int GetOutputBufferSize( unsigned int inputSize )
+    {
+        double chanSamplesPerByte = GetChanSamplesPerByte();
+        return (unsigned int)(inputSize)* chanSamplesPerByte*(m_isComplex ? 2 : 1);
+    }
+
+    /**
+     * Unpack
+     */
+    void Unpack(std::vector<uint8_t> &inputBuffer, std::vector<std::vector<int8_t> > &outputBuffers)
+    {        
+        unsigned int outputIdx = 0;        
+
+        // Verify output Size.
+        unsigned int outputSize = GetOutputBufferSize(inputBuffer.size());
+        for(auto iter = outputBuffers.begin(); iter != outputBuffers.end(); iter++)
+        {
+            if(outputSize != (*iter).size())
+            {
+                throw std::runtime_error("Output buffer size incorrect.");
+            }
+        }
+
+        // Verify output Buffer is the correct size.
+        // For each bytes.
+        for (unsigned int i = 0; i < inputBuffer.size(); i++)
+        {
+            unsigned char byte = inputBuffer[i];
+            for (unsigned int j = 0; j < 8 / m_numBits; j++)
+            {
+                unsigned char point = (byte >> (m_numBits * j)) & 0x3;
+
+                int samplePoint = m_map[point];                
+                outputBuffers[m_channelIdx][outputIdx+m_componentIdx]=(samplePoint);
+
+                if (m_isComplex)
+                {
+                    m_componentIdx = (m_componentIdx + 1) % 2;
+                    if (m_componentIdx == 0)
+                    {
+                        m_channelIdx = (m_channelIdx + 1) % m_numChannels;                        
+                        if(m_channelIdx==0)
+                            outputIdx = outputIdx+(m_isComplex?2:1);
+                    }
+                }
+            }
+        }
+    }
+
+  private:
+    std::vector<int> m_map;
+    unsigned int m_numChannels;
+    unsigned int m_numBits;
+    bool m_isComplex;
+    unsigned int m_channelIdx;
+    unsigned int m_componentIdx;
+    unsigned int m_outputBufferSize;
+};
+
+#endif
