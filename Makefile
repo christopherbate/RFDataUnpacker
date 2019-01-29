e91d93ed6cc55f410da66b8b97dbea790e3a7568
chris <christopher.t.bate1@gmail.com>
2019-01-29 15:58:17 -0700

    Updated makefile, formatting, and directory structure

--- Makefile
+++ Makefile
@@ -1,9 +1,34 @@
 CC=g++
+CXX_FLAGS= -Wall -Werror -O3 -I./QuickTestCPP -I./src
+LD_FLAGS=
 
-all: unpacker test
+BIN_DIR=./bin
+SRC_DIR=./src
+TEST_DIR=./test
+X86_BUILD_DIR=./.build-x86
 
-unpacker: unpacker.cpp
-	$(CC) -o unpacker unpacker.cpp -Wall -Werror
+SRC_FILES=$(wildcard $(SRC_DIR)/*.cpp)
+OBJ_FILES=$(patsubst $(SRC_DIR)/%.cpp,$(X86_BUILD_DIR)/%.o,$(SRC_FILES))
 
-test: test.cpp
-	$(CC) -o test test.cpp -Wall -Werror
+APP_OBJ_FILES=$(filter-out $(X86_BUILD_DIR)/test.o, $(OBJ_FILES))
+TEST_OBJ_FILES=$(filter-out $(X86_BUILD_DIR)/main.o, $(OBJ_FILES))
+
+all: $(BIN_DIR)/unpacker $(BIN_DIR)/test
+
+$(BIN_DIR)/unpacker: $(X86_BUILD_DIR) $(BIN_DIR) $(APP_OBJ_FILES)
+	$(CC) $(APP_OBJ_FILES) $(LD_FLAGS) -o $@
+
+$(BIN_DIR)/test: $(X86_BUILD_DIR) $(BIN_DIR) $(TEST_OBJ_FILES)
+	$(CC) $(TEST_OBJ_FILES) $(LD_FLAGS) -o $@
+
+$(BIN_DIR):
+	mkdir $@
+
+$(X86_BUILD_DIR):
+	mkdir $@
+
+$(X86_BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp
+	$(CC) $(CXX_FLAGS) -c $< -o $@
+
+clean:
+	rm -rf $(BIN_DIR) $(X86_BUILD_DIR)
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
+#include "Unpacker.hpp"
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#include "unpacker.hpp"
 
 using namespace std;
 
-int main()
-{
+int main() {
     // Default mapping.
     std::vector<int> map = {1, 3, -3, -1};
     bool complex = true;
@@ -20,42 +19,35 @@ int main()
     vector<vector<int8_t>> outputBuffers;
     const unsigned int bufferSize = 1024000;
 
-    try
-    {
+    try {
         // Process the settings file.
         std::ifstream infile;
         infile.open("settings.txt", ifstream::in);
-        while (!infile.eof())
-        {
+        while (!infile.eof()) {
             string key;
             string value;
             string value2;
             infile >> key;
-            if (key == "bits")
-            {
+            if (key == "bits") {
                 infile >> value;
                 bits = stoul(value);
             }
-            if (key == "channels")
-            {
+            if (key == "channels") {
                 infile >> value;
                 channels = stoul(value);
             }
-            if (key == "map")
-            {
+            if (key == "map") {
                 infile >> value;
                 infile >> value2;
             }
-            if (key == "file")
-            {
+            if (key == "file") {
                 infile >> file;
                 cout << "Input: " << file << std::endl;
             }
-            if (key == "out")
-            {
+            if (key == "out") {
                 infile >> value;
                 channelFilenames.push_back(value);
-                std::cout << "Added " << value << " for output " << std::endl;               
+                std::cout << "Added " << value << " for output " << std::endl;
             }
         }
         infile.close();
@@ -64,85 +56,83 @@ int main()
         std::cout << "Channels " << channels << endl
                   << "Bits " << bits << endl
                   << "Complex " << complex << endl;
-        std::cout << "Map:" << endl;        
-        for (auto i = 0; i < 4; i++)
-        {
+        std::cout << "Map:" << endl;
+        for (auto i = 0; i < 4; i++) {
             std::cout << i << ": " << (signed)map[i] << std::endl;
         }
 
         //   Create unpacker.
-        Unpacker unpacker( map, channels, bits ,true);
-        
+        Unpacker unpacker(map, channels, bits, true);
+
         // Verify number of output files is correct.
-        if (channelFilenames.size() != channels)
-        {
-            throw std::runtime_error("Must specify the same number of channels as output filenames");
+        if (channelFilenames.size() != channels) {
+            throw std::runtime_error(
+                "Must specify the same number of channels as output filenames");
         }
 
         // Open output files and buffers.
         outFiles.resize(channels);
         outputBuffers.resize(channels);
         unsigned int outIdx = 0;
-        for(auto iter = channelFilenames.begin(); iter!=channelFilenames.end();iter++)
-        {
-            outFiles[outIdx].open( (*iter).c_str(), std::ofstream::binary | std::ofstream::out);
-            if(!outFiles[outIdx].is_open()){
+        for (auto iter = channelFilenames.begin();
+             iter != channelFilenames.end(); iter++) {
+            outFiles[outIdx].open((*iter).c_str(),
+                                  std::ofstream::binary | std::ofstream::out);
+            if (!outFiles[outIdx].is_open()) {
                 throw std::runtime_error("Could not open output file.");
             }
-            outputBuffers[outIdx].resize(unpacker.GetOutputBufferSize( bufferSize ));
+            outputBuffers[outIdx].resize(
+                unpacker.GetOutputBufferSize(bufferSize));
             outIdx++;
-        }        
+        }
 
         // Open the input file.
         infile.open(file, ifstream::in | ifstream::binary);
-        if(!infile.is_open()){
-            throw std::runtime_error("Error opening file. Does not exist or cannot be opened.");
+        if (!infile.is_open()) {
+            throw std::runtime_error(
+                "Error opening file. Does not exist or cannot be opened.");
         }
 
         // Create read buffer and unpacker
-        std::vector<uint8_t> readBuffer;        
-        readBuffer.resize(bufferSize);     
+        std::vector<uint8_t> readBuffer;
+        readBuffer.resize(bufferSize);
 
-        
         // Perform unpacking
         unsigned int total = 0;
-        unsigned int lastPrint = 0;                
-        infile.seekg(0,std::ios_base::end);
+        unsigned int lastPrint = 0;
+        infile.seekg(0, std::ios_base::end);
         unsigned int fileSize = infile.tellg();
-        infile.seekg(0,std::ios_base::beg);
-        while (!infile.eof())   
-        {
+        infile.seekg(0, std::ios_base::beg);
+        while (!infile.eof()) {
             // read in the bytes;
-            infile.read((char *)&readBuffer[0], bufferSize);            
+            infile.read((char*)&readBuffer[0], bufferSize);
 
             // Unpack.
             unpacker.Unpack(readBuffer, outputBuffers);
-            total += infile.gcount();            
+            total += infile.gcount();
 
             // Write the data to files.
             auto index = 0;
-            for (auto it = outFiles.begin(); it != outFiles.end(); it++)
-            {
-                (*it).write((char *)outputBuffers[index].data(), outputBuffers[index].size());                
+            for (auto it = outFiles.begin(); it != outFiles.end(); it++) {
+                (*it).write((char*)outputBuffers[index].data(),
+                            outputBuffers[index].size());
                 index++;
-            }                        
-            //Percentage
-            if(total-lastPrint > 1000000000)
-            {
-                std::cout<< total<<" bytes processed, "<< (double)total/(double)fileSize <<" percent complete."<<std::endl;
+            }
+            // Percentage
+            if (total - lastPrint > 1000000000) {
+                std::cout << total << " bytes processed, "
+                          << (double)total / (double)fileSize
+                          << " percent complete." << std::endl;
                 lastPrint = total;
-            }            
+            }
         }
 
         // Cleanup
         infile.close();
-        for(auto iter = outFiles.begin(); iter != outFiles.end(); iter++)
-        {
+        for (auto iter = outFiles.begin(); iter != outFiles.end(); iter++) {
             (*iter).close();
         }
-    }
-    catch (exception &e)
-    {
+    } catch (exception& e) {
         cout << "Exception: " << e.what() << endl;
         return -1;
     }
